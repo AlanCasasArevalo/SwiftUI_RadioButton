83f71b5badf091d3f26ec4f60712443995007237
Alan Casas <alancasasarevalo@gmail.com>
2021-05-15 10:48:35 +0200

    Initial Commit

---
+++ SwiftUI_RadioButton/SwiftUI_RadioButtonApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_RadioButtonApp.swift
+//  SwiftUI_RadioButton
+//
+//  Created by Alan Casas on 15/05/2021.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_RadioButtonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
